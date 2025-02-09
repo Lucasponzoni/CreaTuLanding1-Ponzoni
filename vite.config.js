f161f07d2e375c2123aff7b27a9f80125e2367be
nombredeusuariolucasponzoni <139734105+Lucasponzoni@users.noreply.github.com>
2025-02-08 21:19:40 -0300

    Actualizar

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://lucasponzoni.github.io/react-js---Comision-75160---Ponzoni-Lucas/",
 })
