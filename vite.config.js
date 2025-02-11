24e556b770da3a76842aa024a0e7bece4f958fdb
nombredeusuariolucasponzoni <139734105+Lucasponzoni@users.noreply.github.com>
2025-02-11 18:50:14 -0300

    Actualizar

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://lucasponzoni.github.io/react-js---Comision-75160---Ponzoni-Lucas/",
+  base: "https://lucasponzoni.github.io/CreaTuLanding1-Ponzoni",
 })
